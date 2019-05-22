ac76d34a02978895b786617d00dd050439ecf46d
Brandon Hernandez <starkiller127.bh@gmail.com>
2019-05-22 16:49:56 -0700

    Fixed keyboard polling/input

--- keyboard.py
+++ keyboard.py
@@ -1,47 +1,32 @@
 import musicbox
 import pygame
-import time
 
 pygame.init()
 screen = pygame.display.set_mode((100, 100))
 
 m = musicbox.MusicBox()
-frequency = 100
-lowest_note = 30
-key_order = []
+frequency = 100  # For repetition of note (delay effect)
+lowest_note = 30  # Lowest note that can be played
 
-note = {8: 81, 9: 55, 13: 54, 45: 79, 47: 40, 48: 78, 39: 53, 44: 38, 301: 42, 46: 39, 303: 41, 304: 30, 49: 70, 50: 71, 51: 72, 52: 73, 53: 74, 54: 75, 56: 76, 57: 77, 59: 52, 61: 80, 91: 66, 92: 68, 93: 67, 96: 69, 97: 43, 98: 35, 99: 33, 100: 45, 101: 58, 102: 46, 103: 47, 104: 48, 105: 63, 106: 49, 107: 50, 108: 51, 109: 37, 110: 36, 111: 64, 112: 65, 113: 56, 114: 59, 115: 44, 116: 60, 117: 62, 118: 34, 119: 57, 120: 32, 121: 61, 122: 31}
+'''Default order of notes on keyboard (Don't worry, I did not manually type this out. I'm a programmer. I'm lazy)'''
+note = {8: 81, 9: 54, 13: 53, 47: 40, 48: 78, 39: 52, 44: 38, 45: 79, 46: 39, 303: 41, 304: 30, 49: 69, 50: 70, 51: 71, 52: 72, 53: 73, 54: 74, 55: 75, 56: 76, 57: 77, 59: 51, 61: 80, 91: 65, 92: 67, 93: 66, 96: 68, 97: 42, 98: 35, 99: 33, 100: 44, 101: 57, 102: 45, 103: 46, 104: 47, 105: 62, 106: 48, 107: 49, 108: 50, 109: 37, 110: 36, 111: 63, 112: 64, 113: 55, 114: 58, 115: 43, 116: 59, 117: 61, 118: 34, 119: 56, 120: 32, 121: 60, 122: 31}
 
-'''def main():
+def main():
 
+    #map_notes()
+    #print(note)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key in note:
                     m.play_note(note[event.key])
-                #key = note[event.key]
-                #m.play_note(60)
-                #print(event.scancode, event.key, event.unicode)
-                #print(note[event.key])
                 elif event.key == pygame.K_ESCAPE:
                     running = False
             if event.type == pygame.KEYUP:
-                #if event.key in note:
-                m.stop_note(event.key)
-            if event.key == pygame.K_ESCAPE:
-                    running = False
-                elif event.key == pygame.K_UP:
-                    while key_down:
-                        set_frequency(1)
-                        if event.type == pygame.KEYUP:
-                            key_down = False
-                elif event.key == pygame.K_DOWN:
-                    while keystate[pygame.K_DOWN]:
-                        set_frequency(-1)
-                        keystate = pygame.key.get_pressed()
+                if event.key in note:
+                    m.stop_note(note[event.key])  # Remove to get infinite sustain?
 
-'''
 
 def set_frequency(amount):
     global frequency
@@ -49,10 +34,9 @@ def set_frequency(amount):
 
 
 def map_notes():
-    global key_order
     global lowest_note
     note.clear()
-    key_order.clear()
+    key_order = []
     done = False
     while not done:
         for event in pygame.event.get():
@@ -67,6 +51,6 @@ def map_notes():
         j += 1
 
 
-#main()
+main()
 m.close()
 pygame.quit()
